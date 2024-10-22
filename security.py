8aa5439eb4d36782cf208fadfdbc5430cc5bfd1e
starsden <159563356+starsden@users.noreply.github.com>
2024-10-23 01:06:20 +0300

    add 404 refactor login, register

--- security.py
+++ security.py
@@ -2,27 +2,22 @@ from datetime import datetime, timedelta
 from jose import JWTError, jwt
 from passlib.context import CryptContext
 
-# Настройки для хэширования паролей
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
-# Настройки для JWT
-SECRET_KEY = "o7wELp+wMruYwOFbQq7HZNCRrA9bW7t3BNLrkpiiZTASyvBKLVLvAX04vr6VsOri"  # Замените на свой секретный ключ
+SECRET_KEY = "o7wELp+wMruYwOFbQq7HZNCRrA9bW7t3BNLrkpiiZTASyvBKLVLvAX04vr6VsOri"
 ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 30
 
 
 def hash_password(password: str) -> str:
-    """Хэширует пароль с использованием bcrypt."""
     return pwd_context.hash(password)
 
 
 def verify_password(plain_password: str, hashed_password: str) -> bool:
-    """Проверяет, совпадает ли введенный пароль с хэшированным."""
     return pwd_context.verify(plain_password, hashed_password)
 
 
 def create_access_token(data: dict, expires_delta: timedelta = None) -> str:
-    """Создает JWT токен с указанными данными и временем жизни."""
     to_encode = data.copy()
     if expires_delta:
         expire = datetime.utcnow() + expires_delta
@@ -34,7 +29,6 @@ def create_access_token(data: dict, expires_delta: timedelta = None) -> str:
 
 
 def decode_access_token(token: str):
-    """Декодирует JWT токен и возвращает его содержимое."""
     credentials_exception = Exception("Could not validate credentials")
 
     try:
