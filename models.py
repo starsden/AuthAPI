99bf9ba4667a162ce4b03bbcaeb798a3002a6c36
starsden <159563356+starsden@users.noreply.github.com>
2024-10-23 11:21:31 +0300

    add database

--- models.py
+++ models.py
@@ -1,7 +1,23 @@
-from pydantic import BaseModel
+# models.py
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine
+
+DATABASE_URL = "sqlite:///./test.db"
+
+Base = declarative_base()
+engine = create_engine(DATABASE_URL)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, index=True)
+    username = Column(String, unique=True, index=True)
+    email = Column(String, unique=True, index=True)
+    hashed_password = Column(String)
 
 
-class User(BaseModel):
-    username: str
-    email: str
-    hashed_password: str
+def init_db():
+    Base.metadata.create_all(bind=engine)
